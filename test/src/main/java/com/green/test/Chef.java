f4c3f94649ae20d152b483a755c76e14f3550537
kanonphil <xoghkdbwjd@gmail.com>
2026-01-12 16:38:01 +0900

    spring

---
+++ test/src/main/java/com/green/test/Chef.java
@@ -0,0 +1,20 @@
+package com.green.test;
+
+import org.springframework.stereotype.Component;
+
+interface Cook {
+  void cook();
+}
+
+@Component("a")  // 객체 생성 어노테이션
+public class Chef implements Cook {
+  public void cook() {
+    System.out.println("요리 중");
+  }
+}
+
+class KoreanChef implements Cook {
+  public void cook() {
+    System.out.println("학식 요리 중");
+  }
+}
