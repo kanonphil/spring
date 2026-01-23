db394d5e8d7206667eb379e1227d7307cb2ada42
kanonphil <xoghkdbwjd@gmail.com>
2026-01-23 16:29:10 +0900

    update

--- item/src/main/java/com/green/item/dto/ItemDTO.java
+++ item/src/main/java/com/green/item/dto/ItemDTO.java
@@ -1,5 +1,6 @@
 package com.green.item.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ public class ItemDTO {
   private int itemNum;
   private String itemName;
   private int itemPrice;
+  @JsonFormat(pattern = "yyyy-MM-dd")
   private LocalDateTime regDate;
   private String itemIntro;
 }
