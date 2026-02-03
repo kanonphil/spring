51cea9c57434c0982b0acd8ba9edcc29622d642a
kanonphil <xoghkdbwjd@gmail.com>
2026-02-03 15:31:06 +0900

    test

--- restApi_test/src/main/java/com/green/restApi_test/controller/BookController.java
+++ restApi_test/src/main/java/com/green/restApi_test/controller/BookController.java
@@ -9,7 +9,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/books")
 public class BookController {
-  private List<BookDTO> bookList;
+  private final List<BookDTO> bookList;
 
   public BookController() {
     // 책 목록이 저장될 List 생성
@@ -26,15 +26,14 @@ public class BookController {
   // 모든 도서정보 조회
   @GetMapping("")
   public List<BookDTO> getBookList() {
-    System.out.println("모든 도서정보 조회");
+    //System.out.println("모든 도서정보 조회");
     return bookList;
   }
 
   // 하나의 도서정보 조회
   @GetMapping("/{bookNum}")
   public BookDTO getBook(@PathVariable("bookNum") int bookNum) {
-    System.out.println(bookNum + "번 도서정보를 조회합니다.");
-
+    //System.out.println(bookNum + "번 도서정보를 조회합니다.");
     BookDTO result = null;
     for (BookDTO e : bookList) {
       if (e.getBookNum() == bookNum) {
@@ -49,30 +48,27 @@ public class BookController {
   // 하나의 도서정보를 등록 (도서명, 저자, 도서내용, 가격)
   @PostMapping("")
   public void regBook(@RequestBody BookDTO bookDTO) {
-    System.out.println("도서정보를 등록합니다.");
+    //System.out.println("도서정보를 등록합니다.");
     bookList.add(bookDTO);
   }
 
   // 하나의 도서정보를 삭제
   @DeleteMapping("/{bookNum}")
-  public List<BookDTO> deleteBook(@PathVariable("bookNum") int bookNum) {
-    System.out.println("도서 정보를 삭제합니다. 삭제하려는 글 번호: " + bookNum);
-
+  public void deleteBook(@PathVariable("bookNum") int bookNum) {
+    //System.out.println("도서 정보를 삭제합니다. 삭제하려는 글 번호: " + bookNum);
     for (int i = 0; i < bookList.size(); i++) {
       if (bookList.get(i).getBookNum() == bookNum) {
         bookList.remove(i);
         break;
       }
     }
-
-    return bookList;
   }
 
   // 하나의 도서정보에서 도서명, 저자, 도서가격 수정
   @PutMapping("/{bookNum}")
   public void updateBook(@RequestBody BookDTO bookDTO,
                          @PathVariable("bookNum") int bookNum) {
-    System.out.println("도서 정보를 수정합니다. 수정하려는 글 번호: " + bookNum);
+    //System.out.println("도서 정보를 수정합니다. 수정하려는 글 번호: " + bookNum);
     for (BookDTO e : bookList) {
       if (e.getBookNum() == bookNum) {
         e.setBookTitle(bookDTO.getBookTitle());
